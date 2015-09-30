d70eda72cbfe77d58c540890cdab9018d2fbf51a
Administrator <Administrator@scsbwin-287713.ITTPA.EBIZ.VERIZON.COM>
2015-09-29 23:56:44 -0700

    Jsp Pages

---
+++ src/main/java/vz/SearchController.java
@@ -0,0 +1,5 @@
+package vz;
+
+public class SearchController {
+
+}
---
+++ src/test/java/vz/SearchControllerTest.java
@@ -0,0 +1,52 @@
+package vz;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+/**
+ * The class <code>SearchControllerTest</code> contains tests for the class <code>{@link SearchController}</code>.
+ *
+ * @generatedBy CodePro at 9/29/15 9:30 AM
+ * @author Administrator
+ * @version $Revision: 1.0 $
+ */
+public class SearchControllerTest {
+	/**
+	 * Perform pre-test initialization.
+	 *
+	 * @throws Exception
+	 *         if the initialization fails for some reason
+	 *
+	 * @generatedBy CodePro at 9/29/15 9:30 AM
+	 */
+	@Before
+	public void setUp()
+		throws Exception {
+		// add additional set up code here
+	}
+
+	/**
+	 * Perform post-test clean-up.
+	 *
+	 * @throws Exception
+	 *         if the clean-up fails for some reason
+	 *
+	 * @generatedBy CodePro at 9/29/15 9:30 AM
+	 */
+	@After
+	public void tearDown()
+		throws Exception {
+		// Add additional tear down code here
+	}
+
+	/**
+	 * Launch the test.
+	 *
+	 * @param args the command line arguments
+	 *
+	 * @generatedBy CodePro at 9/29/15 9:30 AM
+	 */
+	public static void main(String[] args) {
+		new org.junit.runner.JUnitCore().run(SearchControllerTest.class);
+	}
+}
