6b9133c596ee482dd2e9fe0f3a14ac25d4973dcf
Sebastin Naveen <Admin@Admin-PC>
2015-11-03 07:49:44 +0530

    Api kEY

--- src/main/java/vz/SearchController.java
+++ src/main/java/vz/SearchController.java
@@ -107,8 +107,9 @@ public class SearchController extends HttpServlet {
         		Content content = new Content();
         		   //POJO class as above for standard message format
         		  // content.addRegId("APA91bERQuEQxDtlMdmdGZ96-A3lRfvn_y4Wt0fE55G5rfmG7M93P9lcfFTb4Ruh-ByqngiezG-6-E9_E4JLjE379wxh8jlt1y5z_l1JmMRl7oVU0-xns3Zgn4j-UejDVC_lOWjfp4lw");
-        		content.addRegId("APA91bE40Poia4sfOJpEmCQ6ktGRxcxkFLC0NRYOqZRRdNFZx1jM5LxRAY_ARQ-s45GRMMhkdiJ8g2CuDT5KyhVZCYaSjJLxUyAlez93bvBcvqQuW6Qac4NUqrOfFVj0CDNHqJKiJwQQ");
+        		//content.addRegId("APA91bE40Poia4sfOJpEmCQ6ktGRxcxkFLC0NRYOqZRRdNFZx1jM5LxRAY_ARQ-s45GRMMhkdiJ8g2CuDT5KyhVZCYaSjJLxUyAlez93bvBcvqQuW6Qac4NUqrOfFVj0CDNHqJKiJwQQ");
         		content.addRegId("APA91bGqqyr-X4Wu_MGFk8Mu9bhq8dcuVO62cTWFelPa2LjvgpIxTL3IzsME76pVHwVsCb1C8vZoFjzTzEnwko_B902ou0okU5B35l_5xzeagqd4_TLT8apSYwQ8VSF6oqOakz2_1bBT");
+        		//APA91bGqqyr-X4Wu_MGFk8Mu9bhq8dcuVO62cTWFelPa2LjvgpIxTL3IzsME76pVHwVsCb1C8vZoFjzTzEnwko_B902ou0okU5B35l_5xzeagqd4_TLT8apSYwQ8VSF6oqOakz2_1bBT
         		   content.createData("Title", "Notification Message");
         		   URL url = new URL("https://android.googleapis.com/gcm/send");
         		   conn = (HttpURLConnection) url.openConnection();
