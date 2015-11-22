dee1fd6a0d82ece8c822c1828bf0037afe8d46e6
Sebastin Naveen <Admin@Admin-PC>
2015-11-22 18:18:54 +0530

    TestServlet Push Notification Changes

--- src/main/java/vz/TestServlet.java
+++ src/main/java/vz/TestServlet.java
@@ -77,32 +77,32 @@ public class TestServlet extends HttpServlet {
 		try
 		{
 			
-			
-			try {
-			    String vcap_services = System.getenv("VCAP_SERVICES");
-			    if (vcap_services != null && vcap_services.length() > 0) {
-			        // parsing rediscloud credentials
-			        JsonRootNode root = new JdomParser().parse(vcap_services);
-			        JsonNode rediscloudNode = root.getNode("rediscloud");
-			        JsonNode credentials = rediscloudNode.getNode(0).getNode("credentials");
-
-			        JedisPool pool = new JedisPool(new JedisPoolConfig(),
-			                credentials.getStringValue("hostname"),
-			               12636,
-			                Protocol.DEFAULT_TIMEOUT,
-			                credentials.getStringValue("password"));
-			        Jedis jedis = pool.getResource();
-			        jedis.set("adminRobert", "no");
-			        jedis.set("adminKimberly", "no");
-			      //  String value = jedis.get("foo");
-			        // return the instance to the pool when you're done
-			        pool.returnResource(jedis);
-			    //    response.getWriter().append("redis value: ").append(String.valueOf(value));
-			    }
-			} catch (InvalidSyntaxException ex) {
-			    // vcap_services could not be parsed.
-				response.getWriter().append("Error at: ").append(ex.getMessage());
-			}
+//			
+//			try {
+//			    String vcap_services = System.getenv("VCAP_SERVICES");
+//			    if (vcap_services != null && vcap_services.length() > 0) {
+//			        // parsing rediscloud credentials
+//			        JsonRootNode root = new JdomParser().parse(vcap_services);
+//			        JsonNode rediscloudNode = root.getNode("rediscloud");
+//			        JsonNode credentials = rediscloudNode.getNode(0).getNode("credentials");
+//
+//			        JedisPool pool = new JedisPool(new JedisPoolConfig(),
+//			                credentials.getStringValue("hostname"),
+//			               12636,
+//			                Protocol.DEFAULT_TIMEOUT,
+//			                credentials.getStringValue("password"));
+//			        Jedis jedis = pool.getResource();
+//			        jedis.set("adminRobert", "no");
+//			        jedis.set("adminKimberly", "no");
+//			      //  String value = jedis.get("foo");
+//			        // return the instance to the pool when you're done
+//			        pool.returnResource(jedis);
+//			    //    response.getWriter().append("redis value: ").append(String.valueOf(value));
+//			    }
+//			} catch (InvalidSyntaxException ex) {
+//			    // vcap_services could not be parsed.
+//				response.getWriter().append("Error at: ").append(ex.getMessage());
+//			}
 			
 			MessageBean msgBean = new MessageBean();
     		msgBean.setCustomerName(request.getParameter("customerName"));
@@ -117,35 +117,35 @@ public class TestServlet extends HttpServlet {
 //    		c.createData("msgTech", "Fios Dispatch Job");
     		       		
     	
-//    		Content content = new Content();
-//    		   //POJO class as above for standard message format
-//    		  // content.addRegId("APA91bERQuEQxDtlMdmdGZ96-A3lRfvn_y4Wt0fE55G5rfmG7M93P9lcfFTb4Ruh-ByqngiezG-6-E9_E4JLjE379wxh8jlt1y5z_l1JmMRl7oVU0-xns3Zgn4j-UejDVC_lOWjfp4lw");
-//    		//content.addRegId("APA91bERQuEQxDtIMdmdGZ96-A3IRfvn_y4Wt0fE55G5rfmG7M93P9IcfFTb4Ruh-ByqngiezG-6-E9_E4JLjE379wxh8jIt1y5z_I1JmMRl7oVU0-xns3Zgn4j-UejDVC_lOWjfp4lw");
-//    		content.addRegId("APA91bHhEh3JBNr8siLLZKzzcTXWEG8xOVs7QEDcqXF9wyBX5d0DOhry8N73infE9TCG8Bm2aJAmlBaA9PwA0qmiomoFXd2zP_A-RluiUHigWUbPGkr2CLTCXzHxS5O04yABbMiuKK0U");
-//    		//APA91bHhEh3JBNr8siLLZKzzcTXWEG8xOVs7QEDcqXF9wyBX5d0DOhry8N73infE9TCG8Bm2aJAmlBaA9PwA0qmiomoFXd2zP_A-RluiUHigWUbPGkr2CLTCXzHxS5O04yABbMiuKK0U
-//    		   content.createData("Title", "Notification Message");
-//    		   URL url = new URL("https://android.googleapis.com/gcm/send");
-//    		   conn = (HttpURLConnection) url.openConnection();
-//    		   conn.setRequestMethod("POST");
-//    		   conn.setRequestProperty("Content-Type", "application/json");
-//    		   conn.setRequestProperty("Authorization", "key="+apiKey);
-//    		   conn.setDoOutput(true);
-//    		   ObjectMapper mapper = new ObjectMapper();
-//    		   DataOutputStream wr = new DataOutputStream(conn.getOutputStream());
-//    		   mapper.writeValue(wr, content);
-//    		   wr.flush();
-//    		   wr.close();
-//    		  int responseCode = conn.getResponseCode();
-//    		  BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-//				String inputLine;
-//				StringBuffer responseBuff = new StringBuffer();
-//				while((inputLine=in.readLine())!= null)
-//				{
-//					responseBuff.append(inputLine);
-//				}
-//				in.close();
-			//	response.getWriter().append("Response Code: ").append(String.valueOf(responseCode));
-		//	response.getWriter().append("Served at: ").append(responseBuff.toString());
+    		Content content = new Content();
+    		   //POJO class as above for standard message format
+    		  // content.addRegId("APA91bERQuEQxDtlMdmdGZ96-A3lRfvn_y4Wt0fE55G5rfmG7M93P9lcfFTb4Ruh-ByqngiezG-6-E9_E4JLjE379wxh8jlt1y5z_l1JmMRl7oVU0-xns3Zgn4j-UejDVC_lOWjfp4lw");
+    		//content.addRegId("APA91bERQuEQxDtIMdmdGZ96-A3IRfvn_y4Wt0fE55G5rfmG7M93P9IcfFTb4Ruh-ByqngiezG-6-E9_E4JLjE379wxh8jIt1y5z_I1JmMRl7oVU0-xns3Zgn4j-UejDVC_lOWjfp4lw");
+    		content.addRegId("APA91bHhEh3JBNr8siLLZKzzcTXWEG8xOVs7QEDcqXF9wyBX5d0DOhry8N73infE9TCG8Bm2aJAmlBaA9PwA0qmiomoFXd2zP_A-RluiUHigWUbPGkr2CLTCXzHxS5O04yABbMiuKK0U");
+    		//APA91bHhEh3JBNr8siLLZKzzcTXWEG8xOVs7QEDcqXF9wyBX5d0DOhry8N73infE9TCG8Bm2aJAmlBaA9PwA0qmiomoFXd2zP_A-RluiUHigWUbPGkr2CLTCXzHxS5O04yABbMiuKK0U
+    		   content.createData("Title", "Notification Message");
+    		   URL url = new URL("https://android.googleapis.com/gcm/send");
+    		   conn = (HttpURLConnection) url.openConnection();
+    		   conn.setRequestMethod("POST");
+    		   conn.setRequestProperty("Content-Type", "application/json");
+    		   conn.setRequestProperty("Authorization", "key="+apiKey);
+    		   conn.setDoOutput(true);
+    		   ObjectMapper mapper = new ObjectMapper();
+    		   DataOutputStream wr = new DataOutputStream(conn.getOutputStream());
+    		   mapper.writeValue(wr, content);
+    		   wr.flush();
+    		   wr.close();
+    		  int responseCode = conn.getResponseCode();
+    		  BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+				String inputLine;
+				StringBuffer responseBuff = new StringBuffer();
+				while((inputLine=in.readLine())!= null)
+				{
+					responseBuff.append(inputLine);
+				}
+				in.close();
+				response.getWriter().append("Response Code: ").append(String.valueOf(responseCode));
+			response.getWriter().append("Served at: ").append(responseBuff.toString());
     		 
 		}catch(MalformedURLException e){e.printStackTrace();response.getWriter().append("Error at: ").append(e.getMessage());}
 		catch (IOException e) {
